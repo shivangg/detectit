58a2270ff306fcbd75b0b4d7196c256f8458a491
shivangg <xshivangx@gmail.com>
2017-11-14 05:30:34 -0800

    added Euclidean clustering, fine-tuned passthrough filter constants

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -23,7 +23,7 @@ def pcl_callback(pcl_msg):
     passthrough = cloud_filtered.make_passthrough_filter() 
     filter_axis = 'z'
     passthrough.set_filter_field_name(filter_axis)
-    axis_min = 0.5
+    axis_min = 0.77
     axis_max = 1.2
     passthrough.set_filter_limits(axis_min, axis_max)
     cloud_filtered = passthrough.filter()
@@ -48,28 +48,54 @@ def pcl_callback(pcl_msg):
     extracted_outliers = cloud_filtered.extract(inliers, negative = True)
     extracted_inliers = cloud_filtered.extract(inliers, negative = False)
     
-    # finally using the filter to ge the resultant point cloud
-    
-    # RANSAC plane segmentation
-
-    # creating the segmenter object
-    
-
-    # call the segment function to obtain the inlier indices and model coefficients
-    
-
     # TODO: Euclidean Clustering
+    white_cloud = XYZRGB_to_XYZ(extracted_outliers)
+    tree = white_cloud.make_kdtree()
 
+    # create a cluster extraction object
+
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # set tolerance for distance threshold
+    # as well as max. and min. cluster size(in points)
+
+    ec.set_ClusterTolerance(0.02)
+    ec.set_MinClusterSize(6)
+    ec.set_MaxClusterSize(250000)
+
+    # search the kd tree for clusters
+    ec.set_SearchMethod(tree)
+    # extract indices for each of the discovered cluster
+    cluster_indices = ec.Extract()
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+
+    cluster_color = get_color_list(len(cluster_indices))
+
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                            white_cloud[indice][1],
+                                            white_cloud[indice][2],
+                                            rgb_to_float(cluster_color[j])
+                                        ])
+
+    # create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
     # TODO: Convert PCL data to ROS messages
     
     pcl_to_ros_objects = pcl_to_ros(extracted_outliers)
     pcl_to_ros_table = pcl_to_ros(extracted_inliers)
+    
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
     # TODO: Publish ROS messages
     pcl_objects_pub.publish(pcl_to_ros_objects)
     pcl_table_pub.publish(pcl_to_ros_table)
+
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
 
 if __name__ == '__main__':
@@ -83,6 +109,7 @@ if __name__ == '__main__':
     # TODO: Create Publishers
     pcl_objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size = 1)
     pcl_table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size = 1)
+    pcl_cluster_pub = rospy.Publisher('/pcl_cluster', PointCloud2, queue_size = 1)
 
     # Initialize color_list
     get_color_list.color_list = []
